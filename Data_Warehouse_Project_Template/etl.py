f291fd44e6fb01ae2152ad376c86f74bafbb801b
Hazem Fathy <hazemmm77773@gmail.com>
2021-06-10 17:18:24 +0200

    fourth

--- Data_Warehouse_Project_Template/etl.py
+++ Data_Warehouse_Project_Template/etl.py
@@ -2,7 +2,6 @@ import configparser
 import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
 
-
 def load_staging_tables(cur, conn):
     for query in copy_table_queries:
         cur.execute(query)
@@ -21,7 +20,7 @@ def main():
 
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
-    
+
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
 
--- Data_Warehouse_Project_Template/sql_queries.py
+++ Data_Warehouse_Project_Template/sql_queries.py
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS staging_events(
                        ,auth text
                        ,firstName text
                        ,gender text
-                        ,itemInSession bigint
+                        ,itemInSession INT8
                         ,lastName text
                         ,length  varchar(50)
                          ,level text
@@ -34,29 +34,32 @@ CREATE TABLE IF NOT EXISTS staging_events(
                                     ,sessionId int
                                      ,song text
                       ,status text
-                      ,ts bigint
+                      ,ts TIMESTAMP
                       ,userAgent text
                       ,userId int
                      )
 """)
 
-staging_songs_table_create = ("""
-CREATE TABLE IF NOT EXISTS staging_songs(
-                      num_songs int,
-                    artist_name varchar(max),
-                     artist_latitude numeric,
-                     artist_longitude numeric,
-                    artist_location varchar(max),
-                  song_id text,
-                    title text,
-                   duration numeric,
-                    year int)
-           """)
+staging_songs_table_create = ("""CREATE TABLE staging_songs
+
+                             (
+                              num_songs INTEGER,
+                              artist_id VARCHAR(20),
+                              artist_latitude NUMERIC(9,6),
+                              artist_longitude NUMERIC(9,6),
+                              artist_location VARCHAR(256),
+                             artist_name VARCHAR(256),
+                             song_id VARCHAR(20),
+                             title VARCHAR(256),
+                            duration NUMERIC(10,6),
+                             year INT
+                            );
+""")
 
 songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS songplays (songplay_id  int IDENTITY(0,1)
-                         ,start_time time NOT NULL
-                         ,user_id int NOT NULL
+                         ,start_time TIMESTAMP NOT NULL
+                         ,user_id int
                          ,artist_name varchar
                          ,level varchar
                          ,song_id varchar
@@ -91,7 +94,7 @@ CREATE TABLE IF NOT EXISTS artists(artist_id varchar PRIMARY KEY
 """)
 
 time_table_create = ("""
-CREATE TABLE IF NOT EXISTS time (start_time time PRIMARY KEY
+CREATE TABLE IF NOT EXISTS time (start_time TIMESTAMP PRIMARY KEY
                      ,hour varchar
                      ,day int
                      ,week int
@@ -105,6 +108,7 @@ CREATE TABLE IF NOT EXISTS time (start_time time PRIMARY KEY
 staging_events_copy = ("""copy staging_events from '{}'
  credentials 'aws_iam_role={}'
  region 'us-west-2'
+
  COMPUPDATE OFF STATUPDATE OFF
  JSON '{}'""").format(config.get('S3','LOG_DATA'),
 
@@ -129,31 +133,31 @@ staging_songs_copy=("""copy staging_songs from '{}'
 
 # FINAL TABLES
 
-songplay_table_insert = ("""INSERT INTO songplay(start_time ,
+songplay_table_insert = ("""INSERT INTO songplays(start_time ,
                                   user_id,level,
                                   song_id,artist_id,
                                   session_id ,
                                   user_agent ,
                                 location )
-                            select  se.ts,
+                            SELECT DISTINCT TIMESTAMP 'epoch' + (se.ts / 1000) * INTERVAL '1 second' as start_time,
                             se.userId,se.level,
                             ss.song_id,ss.artist_id,
                             se.sessionId,se.userAgent,
                             se.location
                             from staging_events se
-                            join staging_songs ss
+                            left join staging_songs ss
                             on se.artist=ss.artist_name
                             and
                             se.song=ss.title
                             and
                             se.length=ss.duration
-                            where WHERE se.page='NextSong'
+                            WHERE se.page='NextSong' and se.userId is NOT NULL
                             """)
 
 user_table_insert = ("""INSERT INTO users(user_id, first_name,last_name ,gender ,level )
                      select userId, firstName,lastName,gender,level
-                     from staging_events
-                     ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level;
+                     from staging_events where  userId is NOT NULL
+
 
 """)
 
@@ -161,27 +165,27 @@ song_table_insert = ("""
 INSERT INTO songs (song_id,title,artist_id,year,duration)
                     select   song_id,title,artist_id,year,duration
                     from staging_songs
-                     ON CONFLICT (song_id)  DO NOTHING ;
+
 """)
 
 artist_table_insert = ("""INSERT INTO artists(artist_id,name,location ,latitude,longitude)
                        select artist_id,artist_name,artist_location,
                        artist_latitude,artist_longitude
                        from  staging_songs
-                       ON CONFLICT (artist_id)  DO UPDATE SET location=EXCLUDED.location
+
 """)
 
 time_table_insert = ("""INSERT INTO time (start_time,hour,day,week,month,year,weekday)
-                     select   ts, extract(hour,ts),
-                     extract(day,ts),extract(week,ts)
-                     ,extract(month,ts),extract(year,ts)
-                     ,extract(weekday,ts) from stg_events
-                     ON CONFLICT (start_time) DO NOTHING;
+                     select   ts, extract(hour from ts),
+                     extract(day from ts),extract(week from ts)
+                     ,extract(month from ts),extract(year from ts)
+                     ,extract(weekday from ts) from staging_events
+
 """)
 
 # QUERY LISTS
 
 create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
-copy_table_queries = [ staging_events_copy,staging_songs_copy]
+copy_table_queries = [staging_songs_copy,staging_events_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
